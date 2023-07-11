2a355b53d11ce4460eb5a714d331a5d3f6488d7c
krnick <sungboss2004@gmail.com>
2023-07-11 23:16:41 +0800

    update module

---
+++ cmd/peak/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/krnick/peak/crawler"
+)
+
+func startCrawling(url string, threshold float64) error {
+
+	ticker := time.NewTicker(time.Minute)
+
+	for ; ; <-ticker.C {
+		// latest one
+		currencyList, err := crawler.CrawlCurrencyInBus(url)
+		if err != nil {
+			log.Println(err)
+			return errors.New("crawl currency failed")
+		}
+
+		currencyList[len(currencyList)-1].PrintCurrency(threshold)
+	}
+
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./peak <number-for-greaterthan>")
+		fmt.Println("E.g.: ./peak 33")
+		return
+	}
+
+	numGreaterThan, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		return
+	}
+
+	go startCrawling("https://rate.bot.com.tw/xrt/quote/day/EUR?Lang=en-US", numGreaterThan)
+
+	select {}
+
+}
